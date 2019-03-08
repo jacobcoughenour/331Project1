d83f1cf060c1f3efa488db292e7df42c8b14cb7a
jacobcoughenour <jacob15545@yahoo.com>
2019-03-08 13:32:38 -0500

    added routes

--- app.js
+++ app.js
@@ -4,4 +4,24 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("hello world"));
 
+app.get('/students', (req, res) => {
+
+});
+
+app.post('/students', (req, res) => {
+
+});
+
+app.put('/students', (req, res) => {
+
+});
+
+app.get('/students/:studentId', (req, res) => {
+
+});
+
+app.delete('/students/:studentId', (req, res) => {
+
+});
+
 app.listen(port, () => console.log(`listening on port ${port}`));
